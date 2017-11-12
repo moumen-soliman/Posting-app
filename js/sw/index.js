34b9eecc204c007b76b62be5bb22d222b7146a63
moumensoliman <moumensoliman@gmail.com>
2017-11-12 15:06:49 +0200

    Add Pollyfills & Service worker

---
+++ js/sw/index.js
@@ -0,0 +1,40 @@
+var staticCacheName = 'posting-v2';
+var allCaches = [
+  staticCacheName,
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '../main/vue.js',
+        '../main/main.js',
+        '../../css/bulma.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('posting-') &&
+                 !allCaches.includes(cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
